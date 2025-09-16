cfc60847803179393afa7fe5f7937eb4ea61c0c5
nguyenyou <tunt081295@gmail.com>
2025-09-16 11:51:51 +0700

    🚢 ship it

--- www/src/www/App.scala
+++ www/src/www/App.scala
@@ -2,10 +2,12 @@ package www
 
 import org.scalajs.dom
 import com.raquo.laminar.api.L.*
+import www.tw.*
 
 case class App() {
   def apply(): HtmlElement = {
     div(
+      tw.flex,
       h1("Hello World!")
     )
   }
