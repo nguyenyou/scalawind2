c4b088c2e68c312c1a920088b3936112c253c47f
nguyenyou <tunt081295@gmail.com>
2025-09-16 11:54:50 +0700

    🚢 ship it

--- www/test/src/MainTests.scala
+++ www/test/src/MainTests.scala
@@ -1,19 +1,15 @@
 package www
 
 import www.Main
+import www.tw.*
 
 import utest.*
 
 object FooTests extends TestSuite {
   def tests = Tests {
     test("hello") {
-      val result = Main.hello()
-      assert(result.startsWith("Hello"))
-      result
-    }
-    test("world") {
-      val result = Main.hello()
-      assert(result.endsWith("World"))
+      val result = tw.flex.css
+      assert(result == "flex")
       result
     }
   }
